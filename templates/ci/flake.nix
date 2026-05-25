8e2fcbc2c1726a678acb9c76c21824c5345a04b5
Jason Bowman <jason@json64.dev>
2026-05-25 15:26:42 -0700

    ci: add checks output to CI flake

--- templates/ci/flake.nix
+++ templates/ci/flake.nix
@@ -30,6 +30,26 @@
     in
     {
       inherit tests;
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          assertTests = lib.mapAttrsToList (
+            suite: subtests:
+            lib.mapAttrsToList (
+              name: t:
+              if t.expr == t.expected then true
+              else throw "FAIL ${suite}.${name}: got ${builtins.toJSON t.expr}, expected ${builtins.toJSON t.expected}"
+            ) subtests
+          ) tests;
+        in
+        {
+          default = pkgs.runCommand "gen-schema-tests" { } ''
+            echo "${toString (builtins.length (lib.flatten assertTests))} tests passed"
+            touch $out
+          '';
+        }
+      );
       devShells = forAllSystems (
         system:
         let
